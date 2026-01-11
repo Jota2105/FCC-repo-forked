325f46dc9793c83f028836cb01780c1b49b14cf8
Juan Ocaña <juandy.099@gmail.com>
2026-01-11 17:44:18 -0500

    Agregar la funcion del modulo de donacion

--- servidor-fc/server/src/models/index.models.js
+++ servidor-fc/server/src/models/index.models.js
@@ -1,10 +1,12 @@
 const setupHistoriaClinicaModels = require('./historiaclinica.models');
 const setupComunidadModels = require('./comunidad.models');
+const setupDonacionesModels = require('./donaciones.models/index')
 const setupHooks = require('./hooks');
 
 function setupModels(sequelize) {
   setupHistoriaClinicaModels(sequelize);
   setupComunidadModels(sequelize);
+  setupDonacionesModels(sequelize)
 
   // Configurar hooks después de inicializar los modelos
   setupHooks();
